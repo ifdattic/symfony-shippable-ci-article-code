33a8fabc2f8408a3bba912eba2c557a668c8dcbd
Andrew Marcinkevicius <andrew.web@ifdattic.com>
2015-08-22 18:20:23 +0300

    Add Uuid identity value object

--- src/Domain/Core/Spec/TestValues.php
+++ src/Domain/Core/Spec/TestValues.php
@@ -10,4 +10,7 @@ final class TestValues
     const ASSERTION_FAILED_MESSAGE = 'Assertion Failed';
     const ASSERTION_FAILED_CODE    = 13;
     const ASSERTION_FAILED_VALUE   = 4;
+
+    const INVALID_UUID = '123';
+    const UUID         = '5399dbab-ccd0-493c-be1a-67300de1671f';
 }
